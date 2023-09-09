1616088fbc88038f3ad5905d831cef1245af3442
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-09 21:10:26 +0100

    Create Build Output/external website

---
+++ Run/Build Next.js
@@ -0,0 +1,6 @@
+ Nextjs: be more creative 
+   renovate: run compatible changes or remix
+     next : builder build unique special 3D object 
+       run: become 2x faster 
+        next: 5x faster 
+          next: 10x faster , creative, innovative,generative,carefully correct errors 
